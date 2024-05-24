d20f032c3d725169a978fa369d1de17afe40ecb7
Erik Gomes <gomeserik35@gmail.com>
2024-05-24 16:54:59 -0300

    metodos exemplo smart tv

---
+++ Java_Fundamentos/metodos/methods/src/SmartTv.java
@@ -0,0 +1,8 @@
+public class SmartTv {
+    boolean Power = false;
+    int chanal = 1;
+    int volume = 20;
+
+    public void ligar()
+
+}
---
+++ Java_Fundamentos/metodos/methods/src/Usuario.java
@@ -0,0 +1,8 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println(smartTv.ligada);
+    }
+}
